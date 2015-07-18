eddb005a11fa0aaa2def5f0922ac2486f922dd61
ASLAN USSAYEV <usprof777@gmail.com>
2015-07-18 05:39:24 -0500

    Add

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Blog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_assets = true
-    config.assets.raise_asset_errors = false
+    
 
     config.assets.initialize_on_precompile = false
   end
